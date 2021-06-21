0a377aba64f2e0f78f9ff529d33d764abaf6822a
AlienDev66 <p.capitango65@gmail.com>
2021-06-21 22:59:12 +0100

    :sparkles: Making button icon component.

---
+++ app/components/ButtonIcon/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import { styles } from "./styles";
+import DiscordImg from "../../assets/discord.png";
+
+type Props = TouchableOpacityProps & {
+  title: string;
+};
+
+function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default ButtonIcon;
